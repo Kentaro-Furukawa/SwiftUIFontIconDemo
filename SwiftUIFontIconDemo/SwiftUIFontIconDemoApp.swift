baf670acbabe9b240c53d13f0378e456a84d3867
Kentaro Furukawa <info@ken89.com>
2023-08-19 17:33:51 +0900

    Initial Commit

---
+++ SwiftUIFontIconDemo/SwiftUIFontIconDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFontIconDemoApp.swift
+//  SwiftUIFontIconDemo
+//
+//  Created by Kentaro Furukawa on 2023/08/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFontIconDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
